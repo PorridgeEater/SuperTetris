65ec4f957b8d94d4e2a23b1788f1eaa6ced5dda3
zxcjerry <zxcjerrygo@gmail.com>
2015-07-16 15:59:07 +0800

    Model最新改正版本

--- project/Super Tetris/dev/model/Model.h
+++ project/Super Tetris/dev/model/Model.h
@@ -3,29 +3,74 @@
 #include <string>
 #include <vector>
 
-class Block
+#define LENGTH	10
+#define WIDTH	20
+
+class BaseShape
 {
 public:
-	Block() throw() : m_x(0), m_y(0), m_color(0)
+
+	BaseShape(bool matrix[4][4]={false},int color=0) 
 	{	
+		m_x = 4;
+		m_y = 0;
+		m_dir = 0;
+		m_color = color;
+		for(int i=0 ;i<4 ;i++)
+			for(int j=0 ;j<4 ;j++)
+				m_matrix[i][j] = matrix[i][j];
 	}
-	Block(const Block& b)
+	BaseShape(const BaseShape& b)
 	{
+		m_dir = b.m_dir;
 		m_x = b.m_x;
 		m_y = b.m_y;
 		m_color = b.m_color;
+
+		for(int i=0 ;i<4 ;i++)
+			for(int j=0 ;j<4 ;j++)
+				m_matrix[i][j] = b.m_matrix[i][j];
 	}
-	Block(Block&& b)
+	BaseShape(BaseShape&& b)
 	{
+		m_dir = b.m_dir;
 		m_x = b.m_x;
 		m_y = b.m_y;
 		m_color = b.m_color;
+
+		for(int i=0 ;i<4 ;i++)
+			for(int j=0 ;j<4 ;j++)
+				m_matrix[i][j] = b.m_matrix[i][j];
+	}
+	BaseShape& operator=(BaseShape&& b)
+	{
+		if (this != &b)
+		{	
+			m_dir = b.m_dir;
+			m_x = b.m_x;
+			m_y = b.m_y;
+			m_color = b.m_color;
+			for(int i=0 ;i<4 ;i++)
+			for(int j=0 ;j<4 ;j++)
+				m_matrix[i][j] = b.m_matrix[i][j];
+		}
+		return *this;
+	}
+	BaseShape& operator=(const BaseShape& b)
+	{
+		if (this != &b)
+		{	
+			m_dir = b.m_dir;
+			m_x = b.m_x;
+			m_y = b.m_y;
+			m_color = b.m_color;
+			for(int i=0 ;i<4 ;i++)
+			for(int j=0 ;j<4 ;j++)
+				m_matrix[i][j] = b.m_matrix[i][j];
+		}
+		return *this;
 	}
 
-	void set_x(int x) throw()
-	{
-		this->m_x = x;
-	}
 	void set_y(int y) throw()
 	{
 		this->m_y = y;
@@ -40,66 +85,28 @@ public:
 	}
 	int get_y() const throw()
 	{
-		return  m_y;
+		return m_y;
 	}
 	int get_color() const throw()
 	{
 		return m_color;
 	}
-	Block& operator=(const Block& b)
-	{
-		if(this != &b){
-			m_x = b.m_x;
-			m_y = b.m_y;
-			m_color = b.m_color;
-		}
-
-		return *this;
-	} 
-	Block& operator=(Block&& b)
-	{
-		if(this != &b){
-			m_x = s.m_x;
-			m_y = s.m_y;
-			m_color = b.m_color;
-		}
-
-		return *this;
-	}
-private:
-	int m_x;
-	int m_y;
-	int m_color;
-};
-
-class BaseShape
-{
-public:
-	BaseShape() throw() : m_dir(0)
-	{	
-	}
-	BaseShape(const BaseShape& b)
-	{
-		m_dir = b.m_dir;
-		for(int i=0 ;i<4 ;i++)
-			for(int j=0 ;j<4 ;j++)
-				m_matrix[i][j] = b.m_matrix[i][j];
-	}
-	BaseShape(BaseShape&& b)
-	{
-		m_dir = b.m_dir;
-		for(int i=0 ;i<4 ;i++)
-			for(int j=0 ;j<4 ;j++)
-				m_matrix[i][j] = static_cast<Block&&>(b.m_matrix[i][j]);
-	}
-
 	void Rotate()
 	{
 		m_dir = (++m_dir) % 4;
 	}
+
+protected:
+	void Switch(int x,int y)			//用于自定义形状
+	{
+		m_matrix[x][y] = !m_matrix[x][y];
+	}
+
 private:
-	Block m_matrix[4][4];
+	bool m_matrix[4][4];
 	int m_dir;
+	int m_x , m_y;
+	int m_color;
 };
 
 class Map
@@ -110,26 +117,82 @@ public:
 	}
 	Map(const Map& p)
 	{
-		for(int i=0 ;i<10 ;i++)
-			for(int j=0 ;j<20 ;j++)
+		for(int i=0 ;i<LENGTH ;i++)
+			for(int j=0 ;j<WIDTH ;j++)
 			{
-				m_map[i][j] = p.m_map[i][j];
+				m_color[i][j] = p.m_color[i][j];
 				m_exist[i][j] = p.m_exist[i][j];
 			}
 	}
 	Map(Map &&p)
 	{
-		for(int i=0 ;i<10 ;i++)
-			for(int j=0 ;j<20 ;j++)
+		for(int i=0 ;i<LENGTH ;i++)
+			for(int j=0 ;j<WIDTH ;j++)
 			{
-				m_map[i][j] = static_cast<Block&&>(p.m_map[i][j]);
+				m_color[i][j] = p.m_color[i][j];
 				m_exist[i][j] = p.m_exist[i][j];
 			}
 	}
 
+	void Merge(BaseShape& shape)		//shape触底后合并
+	{
+		int x = shape.get_x();
+		int y = shape.get_y();
+		int color = shape.get_color();
+		
+		for(int i=0 ;i<4 ;i++)
+			for(int j=0 ;j<4 ;j++)
+			{
+				m_color[x+i][y+j] = color;
+				m_exist[x+i][y+j] = 1;
+			}
+
+	}
+	void Eliminate(int y){			//消除单行y
+		int i, j;
+		for (j=y;j>0;j++)
+			for (i=0;i<LENGTH;i++)
+			{
+				m_color[i][j] = m_color[i][j-1];
+				m_exist[i][j] = m_exist[i][j-1];		
+			}
+		for (i=0;i<LENGTH;i++)
+		{
+			m_color[i][0] = 0;
+			m_exist[i][0] = 0;		
+		}	
+	}
+	Map& operator= (const Map& m)
+	{
+		if(this != &m){
+			for(int i=0 ;i<LENGTH ;i++)
+				for(int j=0 ;j<WIDTH ;j++)
+				{
+					m_color[i][j] = m.m_color[i][j];
+					m_exist[i][j] = m.m_exist[i][j];
+				}
+		}
+		
+		return *this;
+	} 
+	Map& operator= (Map&& m)
+	{
+		if(this != &m){
+			for(int i=0 ;i<LENGTH ;i++)
+				for(int j=0 ;j<WIDTH ;j++)
+				{
+					m_color[i][j] = m.m_color[i][j];
+					m_exist[i][j] = m.m_exist[i][j];
+				}
+		}
+		
+		return *this;
+	} 
+	
+
 private:
-	Block m_map[10][20];
-	int m_exist[10][20];
+	int m_color[LENGTH][WIDTH];
+	bool m_exist[LENGTH][WIDTH];
 };
 
 class Board
@@ -146,9 +209,37 @@ public:
 	Board(Board &&b)
 	{
 		m_points = b.m_points;
-		m_next = static_cast<BaseShape&&>(b.m_next)
+		m_next = static_cast<BaseShape&&>(b.m_next);
 	}
+
+	int get_points() const throw()
+	{
+		return m_points;
+	}
+	BaseShape& get_next() const throw()
+	{
+		return m_next;
+	}
+	Board& operator= (const Board& b)
+	{
+		if(this != &b){
+			m_points = b.m_points;
+			m_next = b.m_next;
+		}
+		
+		return *this;
+	} 
+	Board& operator= (Board&& b)
+	{
+		if(this != &b){
+			m_points = b.m_points;
+			m_next = static_cast<BaseShape&&>(b.m_next);
+		}
+		
+		return *this;
+	} 
+
 private:
 	int m_points;
 	BaseShape m_next;
-}
+};
